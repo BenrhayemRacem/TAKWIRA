efeecbfc8cb8e28e1f9eef275691ecf667fbb351
BenrhayemRacem <racembenrhayem@insat.u-cartage.tn>
2022-06-02 05:01:11 +0100

    prepare deploy cloud

--- src/routes/reservation.route.js
+++ src/routes/reservation.route.js
@@ -4,10 +4,13 @@ const reservationController = require("../controllers/reservation.controller")
 const jwtHandling = require("../services/jwtHandling");
 const UserType = require("../enums/userTypes");
 const reservationGuard = require("../guards/reservation.guard")
+const ownerAndOwnerRequestGuard = require("../guards/ownerAndRequestOwner.guard");
 
 
 router.post("/add" ,[jwtHandling.jwtVerify([UserType.Client]),reservationGuard], reservationController.add)
 router.get("/getForField/:fieldId/:date" , reservationController.getFieldReservation)
 router.get("/free/:fieldId/:date" , reservationController.giveAvailableReservation)
+router.delete("/:id" , reservationController.deleteReservation)
 router.get("/client" ,[jwtHandling.jwtVerify([UserType.Client]),reservationGuard],reservationController.getClientReservations)
+router.get("/owner",[jwtHandling.jwtVerify([UserType.Owner,UserType.OwnerRequest]),ownerAndOwnerRequestGuard],reservationController.getReservationForOwner)
 module.exports = router ;
